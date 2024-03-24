97d685bb65563d9f6509ee6d23bbd71a894e9095
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-03-24 17:03:33 +0100

    update

--- index.js
+++ index.js
@@ -1,64 +1,42 @@
+import * as contactsService from "./contacts.js";
 import { program } from "commander";
-import readline from "readline";
-import {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-} from "./contacts.js";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
   .option("-n, --name <type>", "user name")
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
+
 program.parse(process.argv);
+
 const options = program.opts();
 
+// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      console.table(await listContacts());
-      process.exit();
+      const listContacts = await contactsService.listContacts();
+      console.table(listContacts);
+      break;
 
     case "get":
-      if (!id) {
-        console.log(
-          'To enable "get" action you must provide an id. Please try again.'
-        );
-        process.exit();
-      }
-      console.log(await getContactById(id));
-      process.exit();
+      const getContact = await contactsService.getContactById(id);
+      console.table(getContact);
+      break;
 
     case "add":
-      if (!(email && name && phone)) {
-        console.log(
-          'To enable "add" action you must provide email, name and phone. Please try again.'
-        );
-        process.exit();
-      }
-      console.log(await addContact(name, email, phone));
-      process.exit();
+      const addUsers = await contactsService.addContact(name, email, phone);
+      console.table(addUsers);
+      break;
 
     case "remove":
-      if (!id) {
-        console.log(
-          'To enable "remove" action you must provide an id. Please try again.'
-        );
-        process.exit();
-      }
-      console.log(await removeContact(id));
-      process.exit();
+      const removed = await contactsService.removeContact(id);
+      console.table(removed);
+      break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
-      process.exit();
   }
 }
 
